dbc825e36a1d299b9d3523068d66f23f40625462
jeandev/ <jeancarlocastillon33@gmail.com>
2023-08-30 15:51:37 +0800

    4th commit

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">International State College of the Philippines</p>
+							<h3>
+
+							<p>ISCP  International State College of the Philippines</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>FREE TUITION:</p>
+												<p>We OFFER best of best Education</p>
+												<p>Welcome to International State College of the Philippines</p>
+								</div>		
+						</div>		
+				</div>
